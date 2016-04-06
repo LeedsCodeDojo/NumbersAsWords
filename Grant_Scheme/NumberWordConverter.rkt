ad544dbc501435d1651b8a128264969ecf804a1b
Grant Crofton <grantac@hotmail.com>
2016-04-06 14:20:19 +0100

    A bit more scheme

--- Grant_Scheme/NumberWordConverter.rkt
+++ Grant_Scheme/NumberWordConverter.rkt
@@ -8,20 +8,44 @@
    ))
 
 (define tens 
-  (hash 
+  (hash
+   10 "ten"
    20 "twenty"
    ))
 
+(define replacements 
+  (hash
+   "ten one" "eleven"
+   "ten two" "twelve"
+   ))
+
 ; functions
-(define (lookup map number) (hash-ref map number ""))
+(define (lookup map number suffix)
+  (let ((value (hash-ref map number "")))
+    (cond
+      ((eq? value "") "")
+      (else (string-append value suffix)))))
+
 (define (get-single number) (modulo number 10))
 (define (get-ten number) (- (modulo number 100) (get-single number)))
+(define (get-hundred number) (/ (- number (get-ten number) (get-single number)) 100))
+
+(define (replace-irregular-numbers numberString keys)
+  (cond
+    ((null? keys) numberString)
+    (else
+     (replace-irregular-numbers
+      (string-replace numberString (first keys) (hash-ref replacements (first keys)))
+      (rest keys)))))
 
 (define (number->words number)
-  (string-trim 
-   (string-append 
-    (lookup tens (get-ten number)) 
-    " " 
-    (lookup singles (get-single number)))))
+  (replace-irregular-numbers 
+   (string-trim 
+    (string-append
+     (lookup singles (get-hundred number) " hundred ") 
+     (lookup tens (get-ten number) " ") 
+     (lookup singles (get-single number) "")))
+   (hash-keys replacements)))
+
   
 (provide number->words)
--- Grant_Scheme/NumberWordConverterTests.rkt
+++ Grant_Scheme/NumberWordConverterTests.rkt
@@ -5,11 +5,11 @@
 
 (check-equal? (number->words 1) 
               "one"
-              "single number one")
+              "single number: one")
 
 (check-equal? (number->words 2) 
               "two"
-              "single number two")
+              "single number: two")
 
 (check-equal? (number->words 20) 
               "twenty"
@@ -19,6 +19,17 @@
               "twenty one"
               "complex tens: twenty one")
 
+(check-equal? (number->words 11) 
+              "eleven"
+              "irregular numbers: eleven")
+
+(check-equal? (number->words 12) 
+              "twelve"
+              "irregular numbers: twelve")
+
+(check-equal? (number->words 100) 
+              "one hundred"
+              "hundreds: one hundred")
 
 "**********************"
 "*** Tests Complete ***"
